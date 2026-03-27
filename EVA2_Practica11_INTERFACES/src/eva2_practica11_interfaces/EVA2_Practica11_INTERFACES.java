5f50c7b3535a7e5abe7b13d071c06bac5645c6ca
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2026-03-26 21:20:36 -0600

    EVA2_Practica11_INTERFACES

---
+++ EVA2_Practica11_INTERFACES/src/eva2_practica11_interfaces/EVA2_Practica11_INTERFACES.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica11_interfaces;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_Practica11_INTERFACES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.mostrarMensaje();
+        Mensaje mensaje = new Mensaje();
+    }
+    
+}
+interface Prueba{
+    
+}
